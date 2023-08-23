00d3ea0a46d7bebf0390d83feee03e1ff4a1403e
pablo-caue <pablocrs2004@gmail.com>
2023-08-23 10:21:49 -0300

    feat: challenge finished

--- app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
+++ app/src/main/java/br/com/igorbag/githubsearch/data/GitHubService.kt
@@ -6,7 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
-
     @GET("users/{user}/repos")
     fun getAllRepositoriesByUser(@Path("user") user: String): Call<List<Repository>>
 
